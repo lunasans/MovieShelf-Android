c9bc2384ae20394554fd349f832e86fcafc437ff
Lunasans <rene@rene-neuhaus.eu>
2026-07-19 15:07:25 +0200

    chore(release): 1.10.0 - Staffeln verwalten (versionCode 22)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "at.neuhaus.movieshelf"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.9.0"
+        versionCode = 22
+        versionName = "1.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
